7efb10fe9b14685f606ef0b5f0117d9de39a8997
qquentinv <quentinvoisin00@gmail.com>
2024-09-14 23:48:44 +0200

    feat(plugins): add open tree on start

--- dot_config/nvim/lua/plugins/prettier.lua
+++ dot_config/nvim/lua/plugins/prettier.lua
@@ -1,26 +1,29 @@
 return {
-	"MunifTanjim/prettier.nvim",
-	dependencies = {
-		{"jose-elias-alvarez/null-ls.nvim"}
-	},
-	config = function()
-		local prettier = require("prettier")
-		prettier.setup({
-			bin = 'prettier',
-			filetypes = {
-				"css",
-				"html",
-				"javascript",
-				"javascriptreact",
-				"json",
-				"markdown",
-				"yaml"
-			},
-			cli_options = {
-				semi = true,
-				trailling_coma = "all",
-				single_quote = true,
-			}
-		})
-	end
+  "MunifTanjim/prettier.nvim",
+  dependencies = {
+    { "jose-elias-alvarez/null-ls.nvim" }
+  },
+  config = function()
+    local null_ls = require("null-ls")
+    local prettier = require("prettier")
+
+    -- Configurer Prettier
+    prettier.setup({
+      bin = 'prettier',
+      filetypes = {
+        "css",
+        "html",
+        "javascript",
+        "javascriptreact",
+        "json",
+        "markdown",
+        "yaml"
+      },
+      cli_options = {
+        semi = true,
+        trailing_comma = "all",
+        single_quote = false,
+      }
+    })
+ end
 }
--- dot_config/nvim/lua/plugins/tree.lua
+++ dot_config/nvim/lua/plugins/tree.lua
@@ -1,23 +1,37 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	config = function()
-		local tree = require("nvim-tree")
+  "nvim-tree/nvim-tree.lua",
+  config = function()
+    local tree = require("nvim-tree")
 
-		tree.setup({
-			renderer = {
-				root_folder_label = false,
-				icons = {
-					show = {
-						file = true,
-						folder = true,
-						folder_arrow = true,
-						git = true
-					}
-				}
-			},
-			view = {
-				adaptive_size = true,
-			}
-		})
-	end
+    tree.setup({
+      renderer = {
+        root_folder_label = false,
+        icons = {
+          show = {
+            file = true,
+            folder = true,
+            folder_arrow = true,
+            git = true
+          }
+        }
+      },
+      view = {
+        adaptive_size = true,
+      }
+    })
+
+    -- open nvim tree by default
+		  -- Fonction pour ouvrir nvim-tree
+    local function open_nvim_tree()
+      require("nvim-tree.api").tree.open()
+    end
+
+    -- Créer un autocmd pour ouvrir nvim-tree au démarrage
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function()
+        -- Attendre un moment pour permettre le démarrage complet
+        vim.defer_fn(open_nvim_tree, 100)
+      end
+    })
+  end
 }
